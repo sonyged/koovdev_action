5310862f160c3594e4a4939d90b1e7b1f6253854
enami tsugutomo <t.enami@sonyged.com>
2019-09-11 09:41:29 +0900

    Add test for led and dc-motor.

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@
 const assert = require('assert');
 const rewire = require('rewire');
 const sinon = require('sinon');
+const { promisify } = require('util');
 
 describe('terminate_device without device', () => {
   it('should call callback', (done) => {
@@ -41,5 +42,126 @@ describe('on_disconnect etc', () => {
     ka.on_close(true);
     assert(terminate.calledThrice);
     done();
+  });
+});
+
+const setup_action = () => {
+  const terminate = sinon.spy();
+  const koovdev_action = rewire('../koovdev_action');
+  const koov_actions = koovdev_action.__get__('koov_actions');
+  const board = {
+    digitalWrite: sinon.spy(),
+    analogWrite: sinon.spy(),
+    HIGH: 'HIGH',
+    LOW: 'LOW',
+    on: sinon.spy() };
+
+  koovdev_action.__set__('open_firmata', (action, cb, opts) => {
+    const action_timeout = opts.action_timeout || 60 * 1000;
+    action.board = board;
+    action.action = koov_actions(
+      board, action_timeout, action.selected_device);
+    cb(null);
+  });
+
+  const action = koovdev_action.action({
+    device: {
+      open: (name, cb) => cb(null),
+      close: cb => cb(null),
+      terminate }});
+  const open = promisify((name, cb) => {
+    const v = action.open(name, { callback: cb});
+    return v;
+  });
+  const close = promisify(cb => action.close(cb));
+
+  return {
+    board,
+    action,
+    open,
+    close };
+};
+
+describe('turn_led', () => {
+  it('should call this.terminate_device', async () => {
+    const { action, board, open, close } = setup_action();
+
+    assert.equal(await open('/dev/null'), null);
+    assert(board.on.calledThrice);
+
+    const fn = promisify(action.action['turn-led']);
+
+    assert.equal(await fn({
+      port: 'V2',
+      mode: 'ON'
+    }, null), null);
+    assert(board.digitalWrite.calledOnce);
+    assert.deepEqual(board.digitalWrite.args[0], [10, 'HIGH']);
+
+    assert.equal(await fn({
+      port: 'V3',
+      mode: 'OFF'
+    }, null), null);
+    assert(board.digitalWrite.calledTwice);
+    assert.deepEqual(board.digitalWrite.args[1], [11, 'LOW']);
+
+    assert.equal(await close(), null);
+  });
+});
+
+describe('dcmotor_control', () => {
+  it('should call this.terminate_device', async () => {
+    const { action, board, open, close } = setup_action();
+
+    assert.equal(await open('/dev/null'), null);
+    assert(board.on.calledThrice);
+
+    const set_dcmotor_power = promisify(action.action['set-dcmotor-power']);
+    const turn_dcmotor_on = promisify(action.action['turn-dcmotor-on']);
+    const turn_dcmotor_off = promisify(action.action['turn-dcmotor-off']);
+
+    assert.equal(await set_dcmotor_power({
+      port: 'V0',
+      power: 100                // this value may be an expression
+                                // such as 1 + 2.
+    }, {
+      power: 100                // this is calculated value.
+    }), null);
+    assert(board.digitalWrite.calledOnce);
+    assert.deepEqual(board.digitalWrite.args[0], [ 13, 'LOW' ]);
+    assert(board.analogWrite.calledOnce);
+    assert.deepEqual(board.analogWrite.args[0], [ 12, 0 ]);
+
+    assert.equal(await turn_dcmotor_on({
+      port: 'V0',
+      direction: 'NORMAL'
+    }, null ), null);
+    assert.equal(board.digitalWrite.callCount, 2);
+    assert.deepEqual(board.digitalWrite.args[1], [ 13, 'LOW' ]);
+    assert.equal(board.analogWrite.callCount, 2);
+    assert.deepEqual(board.analogWrite.args[1], [ 12, 254 ]);
+
+    assert.equal(await set_dcmotor_power({
+      port: 'V0',
+      power: 50                 // this value may be an expression
+                                // such as 1 + 2.
+    }, {
+      power: 50                 // this is calculated value.
+    }), null);
+    assert.equal(board.digitalWrite.callCount, 3);
+    assert.deepEqual(board.digitalWrite.args[2], [ 13, 'LOW' ]);
+    assert.equal(board.analogWrite.callCount, 3);
+    assert.deepEqual(board.analogWrite.args[2], [ 12, 107 ]);
+
+    assert.equal(await turn_dcmotor_off({
+      port: 'V0',
+      mode: 'BRAKE'
+    }, null ), null);
+    assert.equal(board.digitalWrite.callCount, 4);
+    assert.deepEqual(board.digitalWrite.args[3], [ 13, 'HIGH' ]);
+    assert.equal(board.analogWrite.callCount, 4);
+    assert.deepEqual(board.analogWrite.args[3], [ 12, 255 ]);
+
+    assert.equal(await close(), null);
   });
 });
